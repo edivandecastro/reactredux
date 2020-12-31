d7aed145f02be6ff16372fae5a8217f9e6856b26
Edivan de Castro <edivandecastro@gmail.com>
2020-12-31 11:16:45 -0300

    Movido o state de módulos para o componente de nível superior App

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,25 +1,6 @@
 import React, { useState } from 'react';
 
-function Sidebar() {
-  const [modules, setModules] = useState([
-    {
-      id: 1,
-      title: "Iniciando com React",
-      lessons: [
-        { id: 1, title: "Primeira aula" },
-        { id: 2, title: "Segunda aula" }
-      ],
-    },
-    {
-      id: 2,
-      title: "Aprendendo Redux",
-      lessons: [
-        { id: 3, title: "Terceira aula" },
-        { id: 4, title: "Quarta aula" }
-      ],
-    },
-  ]);
-
+function Sidebar({ modules }) {
   return(
     <aside>
       { modules.map(module => (
